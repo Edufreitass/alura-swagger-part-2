6776e24f926036ce08ca873c2458480e1ff3531f
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-21 18:53:35 -0300

    Lesson 05 - Customizing the queryExtract operation with POSTMAN

--- src/main/java/io/swagger/api/ExtratoApiController.java
+++ src/main/java/io/swagger/api/ExtratoApiController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,36 +16,45 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.annotations.ApiParam;
+import io.swagger.customizacao.service.ContaService;
+import io.swagger.customizacao.util.RespostasUtil;
 import io.swagger.model.Extrato;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-07-21T12:37:02.908Z")
 
 @Controller
 public class ExtratoApiController implements ExtratoApi {
 
-    private static final Logger log = LoggerFactory.getLogger(ExtratoApiController.class);
+	@Autowired
+	private ContaService contaService;
 
-    private final ObjectMapper objectMapper;
+	@Autowired
+	private RespostasUtil respostasUtil;
 
-    private final HttpServletRequest request;
+	private static final Logger log = LoggerFactory.getLogger(ExtratoApiController.class);
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public ExtratoApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
+	private final ObjectMapper objectMapper;
 
-    public ResponseEntity<Extrato> consultaExtrato(@ApiParam(value = "",required=true) @PathVariable("agencia") Integer agencia,@ApiParam(value = "",required=true) @PathVariable("numero") Long numero,@ApiParam(value = "",required=true) @PathVariable("digito") Integer digito,@ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Extrato>(objectMapper.readValue("{  \"transacoes\" : [ {    \"tipo\" : \"debito\",    \"data\" : \"2000-01-23T04:56:07.000+00:00\",    \"valor\" : 6.027456183070403,    \"id\" : 0  }, {    \"tipo\" : \"debito\",    \"data\" : \"2000-01-23T04:56:07.000+00:00\",    \"valor\" : 6.027456183070403,    \"id\" : 0  } ]}", Extrato.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Extrato>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+	private final HttpServletRequest request;
 
-        return new ResponseEntity<Extrato>(HttpStatus.NOT_IMPLEMENTED);
-    }
+	@org.springframework.beans.factory.annotation.Autowired
+	public ExtratoApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+		this.objectMapper = objectMapper;
+		this.request = request;
+	}
+
+	public ResponseEntity<Extrato> consultaExtrato(
+			@ApiParam(value = "", required = true) @PathVariable("agencia") Integer agencia,
+			@ApiParam(value = "", required = true) @PathVariable("numero") Long numero,
+			@ApiParam(value = "", required = true) @PathVariable("digito") Integer digito,
+			@ApiParam(value = "", required = true) @RequestHeader(value = "Authorization", required = true) String authorization) {
+
+		try {
+			return contaService.consultaExtrato(authorization, agencia, numero, digito);
+		} catch (Exception e) {
+			return respostasUtil.getErroInternoExtrato(RespostasUtil.MENSAGEM_FALHA_AO_TENTAR_CONSULTAR_EXTRATO);
+		}
+
+	}
 
 }
